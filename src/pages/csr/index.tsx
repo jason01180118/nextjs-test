5ef11b96bb106a78b5b365f956fc90000236811b
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2023-04-29 21:45:01 +0800

    feat: add 404 and ssr/ssg/csr example

--- src/pages/csr/index.tsx
+++ src/pages/csr/index.tsx
@@ -1,27 +1,27 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import axios from 'axios';
-import { useEffect } from 'react';
-import WebSocket from 'ws';
-import { sha256 } from 'js-sha256';
+import useSWR from 'swr';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function CSR() {
+  const { data, error } = useSWR('/api/hello', (...args) =>
+    fetch(...args).then((res) => res.json())
+  );
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Link className="text-white" href="/ssr">
-          SSR
-        </Link>
-        <Link className="text-white" href="/ssg">
-          SSG
-        </Link>
-        <Link className="text-white" href="/csr">
-          CSR
+        <p className="text-white">{data.name}</p>
+        <Link className="text-white" href="/">
+          home
         </Link>
       </div>
     </main>
--- src/pages/ssg/index.tsx
+++ src/pages/ssg/index.tsx
@@ -8,20 +8,35 @@ import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export async function getStaticProps() {
+  let testName;
+  await axios.get('http://localhost:3000/api/hello').then((res) => {
+    testName = res.data.name;
+    console.log(res.data.name);
+  });
+
+  return {
+    props: {
+      testName,
+    },
+  };
+}
+
+interface Props {
+  testName: string;
+}
+
+export default function SSG({ testName }: Props) {
+  console.log(testName);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Link className="text-white" href="/ssr">
-          SSR
-        </Link>
-        <Link className="text-white" href="/ssg">
-          SSG
-        </Link>
-        <Link className="text-white" href="/csr">
-          CSR
+        <p className="text-white">{testName}</p>
+        <Link className="text-white" href="/">
+          home
         </Link>
       </div>
     </main>
